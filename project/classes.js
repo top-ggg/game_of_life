d3084690ed25423de6e0baef8700690333bf2fcf
top-ggg <student244@tumo.world>
2022-09-28 18:13:14 +0200

    added flower, season and lightning class in GoL

--- project/classes.js
+++ project/classes.js
@@ -51,10 +51,13 @@ class GrassEater {
             var x = this.directions[i][0];
             var y = this.directions[i][1];
             if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length){
-                if (matrix[y][x] instanceof Grass) {
+                if (matrix[y][x] instanceof Grass || matrix[y][x] instanceof Flower) {
                     foundGrassEater.push(this.directions[i]);
                     this.energy = 15;
-                }   
+                    if (matrix[y][x] instanceof Flower) {
+                        grassTimer = 50
+                    }
+                }
             }
         }
         if (foundGrassEater.length == 0) {
@@ -65,7 +68,6 @@ class GrassEater {
                     if (matrix[y][x] instanceof Empty) {
                         foundGrassEater.push(this.directions[i]);
                     }
-                    
                 }  
             }
             this.energy--
@@ -107,5 +109,99 @@ class Empty {
     constructor(x,y){
         this.x = x;
         this.y = y;
+    }
+}
+
+
+
+
+
+// Updates
+
+class Season {
+    constructor(){
+        this.season = ['spring', 'summer', 'fall', 'winter']
+        this.seasonTimer = 0
+        this.seasonCount = 0
+        this.currentSeason = this.season[this.seasonCount]
+    }
+
+    recheckSeason(){
+        if(this.seasonTimer > 250){
+            this.seasonTimer = 0
+            if(this.seasonCount >= this.season.length-1){
+                this.seasonCount  = 0
+            } else {
+                this.seasonCount++
+            }
+        } else {
+            this.seasonTimer++
+        }
+        this.currentSeason = this.season[this.seasonCount]
+        document.getElementById('season').innerText = "Season: " + this.currentSeason + " / Season Time: " + this.seasonTimer
+    }
+
+    getGrassColor(x){
+        return grassTimer != 0 ? 'red'
+             : this.currentSeason == 'spring' ? 'green'
+             : this.currentSeason == 'summer' ? 'rgb(194, 178, 128)'
+             : this.currentSeason == 'fall'   ? 'darkgreen'
+             : this.currentSeason == 'winter' ? 'white'
+             : 'green' 
+    }
+    
+}
+
+
+
+class Flower{
+    constructor(x,y){
+        this.x = x
+        this.y = y
+    }
+    check(){
+        if(season.currentSeason != 'spring'){
+            matrix[this.y][this.x] = new Empty(this.x,this.y)
+        }
+    }
+}
+
+
+
+class Lightning {
+    constructor(x,y,child,time){
+        this.x = x;
+        this.y = y;
+        this.child = child
+        this.time = time
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x    , this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y    ],
+            [this.x + 1, this.y    ],     
+            [this.x - 1, this.y + 1],
+            [this.x    , this.y + 1],     
+            [this.x + 1, this.y + 1]  
+        ];
+    }
+    chooseCells(){
+        var found = [];
+        for (var i in this.directions) {
+            var x = this.directions[i][0];
+            var y = this.directions[i][1];
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length){
+                found.push(this.directions[i]);
+            }  
+        }
+        return found
+    }
+    strike(){
+        var turnLighting = this.chooseCells()
+        for(var i in turnLighting){
+            var tempx = this.directions[i][0];
+            var tempy = this.directions[i][1];
+            matrix[tempy][tempx] = new Lightning(tempx,tempy,this.child,timer)
+        }
     }
 }
--- project/script.js
+++ project/script.js
@@ -3,7 +3,14 @@ var side = 15;
 var cellSize = 50;
 var emptyCells = [];
 var timer = 0;
-var weather = ['spring', 'summer', 'fall', 'winter']
+var season = new Season()
+var grassSpeed = 3
+var grassTimer = 0
+
+/*-- Audio --*/
+var winAudio = new Audio('content/win.wav');
+var flowerAudio = new Audio('content/flower.wav');
+var lightningAudio = new Audio('content/lightning.wav');
 
 var emptyItems;
 var grassItems;
@@ -29,29 +36,71 @@ function setup(){
 }
 
 function draw(){
-    frameRate(10)
     emptyCells=[];
     timer++
+    if(grassTimer == 50){
+        flowerAudio.play()
+    }
+    if(grassTimer <= 0){
+        grassSpeed = 3
+        grassTimer = 0
+    } else {
+        grassSpeed = 1
+        grassTimer--
+    }
+    if(grassItems == 0){
+        document.getElementById('winTitle').innerHTML = '<i class="fa-solid fa-flag"></i> GrassEaters Won! <i class="fa-solid fa-flag"></i>'
+        document.getElementById('scoreTitle').innerHTML = '<i class="fa-solid fa-clock"></i> Time: ' + timer
+        winAudio.play()
+        noLoop()
+    } 
+    if(grassEaterItems == 0 && getAllCells(Lightning).length == 0 && grassItems != 0){
+        console.log('Lighning Spawned')
+        var tempLightningX = getRandomInt(0,cellSize+1)
+        var tempLightningY = getRandomInt(0,cellSize+1)
+        matrix[tempLightningY][tempLightningX] = new Lightning(tempLightningX, tempLightningY, GrassEater, timer)
+        lightningAudio.play()
+        matrix[tempLightningY][tempLightningX].strike()
+    } 
+
+    season.recheckSeason()
     for(var i=0;i<cellSize;i++){
         for(var j=0;j<cellSize;j++){
             if(matrix[j][i] instanceof Empty){
                 fill('grey')
             }else if(matrix[j][i] instanceof Grass){
-                fill('green')
+                fill(season.getGrassColor())
                 emptyCells.push(matrix[j][i].chooseCells())
                 emptyCells = emptyCells.filter(e => e != null); // Removes unindentified values(happens when green cells don't have empty cells surrounding it)
             }else if(matrix[j][i] instanceof GrassEater){
                 fill('yellow')
                 matrix[j][i].move()
+            }else if(matrix[j][i] instanceof Flower){
+                fill('pink')
+                matrix[j][i].check()
+            }else if(matrix[j][i] instanceof Lightning){
+                fill('blue')
+                if(matrix[j][i].time+2 <= timer){
+                    matrix[j][i] = new GrassEater(i,j,15)
+                    matrix[j][i].move()
+                }
             }
             rect(j*side,i*side,side,side)
         }
     }
-    if (timer % 3 == 0){
+    if (timer % grassSpeed == 0){
         for(var i in emptyCells){
             var x3 = emptyCells[i][0]
             var y3 = emptyCells[i][1]
             matrix[y3][x3]=new Grass(x3,y3);
+        }
+    }
+    if(season.currentSeason == 'spring'){
+        var tempCoords = [getRandomInt(0,cellSize), getRandomInt(0,cellSize)]
+        if(matrix[tempCoords[1]][tempCoords[0]] instanceof Grass){   
+            if(Math.random() < 0.02){
+                matrix[tempCoords[0]][tempCoords[1]] = new Flower   (tempCoords[0],tempCoords[1])
+            }
         }
     }
 
@@ -63,10 +112,10 @@ function draw(){
             if(matrix[i][j] instanceof Empty){
                 emptyItems++
             }
-            if(matrix[i][j] instanceof Grass){
+            else if(matrix[i][j] instanceof Grass){
                 grassItems++
             }
-            if(matrix[i][j] instanceof GrassEater){
+            else if(matrix[i][j] instanceof GrassEater){
                 grassEaterItems++
             }
         }
@@ -80,9 +129,25 @@ function draw(){
 
 
 
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
 
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+}
 
-
-
-
+function getAllCells(val) {
+    var indexes = []
+    for(i = 0; i < matrix.length; i++){
+        for(j = 0; j < matrix[i].length; j++){
+            if(matrix[i][j] instanceof val){
+                indexes.push(matrix[i][j]) 
+            }
+        }
+    }
+    return indexes
+}
 
